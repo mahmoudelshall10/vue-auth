51c0c49bb8f447447df304a92c5ea55a3ed75f4e
elshal <mahmoudelshall10@outlook.com>
2021-07-09 15:59:01 +0200

    third commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(Config('app.env') !== 'local') 
+        {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
